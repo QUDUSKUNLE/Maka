e93c930e4a8d97ec1287704a22e19c42f1c1edef
QUDUSKUNLE <quduskunle@gmail.com>
2023-07-19 12:32:09 +0100

    Clean up tests

--- src/inventory/inventory.service.spec.ts
+++ src/inventory/inventory.service.spec.ts
@@ -1,5 +1,4 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { NotFoundException } from '@nestjs/common';
 import { InventoryService } from './inventory.service';
 import { PrismaService } from '../prisma/prisma.service';
 
@@ -24,45 +23,53 @@ describe('InventoryService', () => {
     expect(spyInventoryService).toBeDefined();
     expect(prisma).toBeDefined();
   });
-  it('should return count of created inventories', async () => {
-    prisma.inventory.createMany = jest.fn().mockReturnValue({ count: 2 });
-    expect(await spyInventoryService.CreateInventory(createInventory)).toEqual({
-      count: 2,
+  describe('CreateInventory', () => {
+    it('should return count of created inventories', async () => {
+      prisma.inventory.createMany = jest.fn().mockReturnValue({ count: 2 });
+      expect(
+        await spyInventoryService.CreateInventory(createInventory),
+      ).toEqual({
+        count: 2,
+      });
+      expect(prisma.inventory.createMany).toHaveBeenCalled();
     });
-    expect(prisma.inventory.createMany).toHaveBeenCalled();
   });
-  it('should return array of inventories', async () => {
-    prisma.inventory.findMany = jest.fn().mockReturnValue([
-      { itemID: '1', itemName: 'item1', quantity: 2 },
-      { itemID: '2', itemName: 'item2', quantity: 4 },
-    ]);
-    await spyInventoryService.GetInventories();
-    expect(prisma.inventory.findMany).toHaveBeenCalled();
-  });
-  it('should return an inventory of a given id', async () => {
-    prisma.inventory.findUnique = jest
-      .fn()
-      .mockReturnValue({ itemID: '1', itemName: 'item1', quantity: 2 });
-    expect((await spyInventoryService.GetInventory(1)).itemID).toBeDefined();
-    expect(prisma.inventory.findUnique).toHaveBeenCalled();
-  });
-  it('should return an updatedInventory of a given id and update data', async () => {
-    prisma.inventory.update = jest
-      .fn()
-      .mockReturnValue({ itemID: '12', itemName: 'item1', quantity: 2 });
-    await spyInventoryService.UpdateInventory({
-      itemID: 12,
-      itemName: 'item1',
-      quantity: 2,
+  describe('GetInventories', () => {
+    it('should return array of inventories', async () => {
+      prisma.inventory.findMany = jest.fn().mockReturnValue([
+        { itemID: '1', itemName: 'item1', quantity: 2 },
+        { itemID: '2', itemName: 'item2', quantity: 4 },
+      ]);
+      await spyInventoryService.GetInventories();
+      expect(prisma.inventory.findMany).toHaveBeenCalled();
     });
-    expect(prisma.inventory.update).toHaveBeenCalled();
   });
-  it('should throw an error item not found with itemID=10000', async () => {
-    spyInventoryService.GetInventory = jest
-      .fn()
-      .mockRejectedValue(new NotFoundException('Item not found.'));
-    return await spyInventoryService
-      .GetInventory(10000)
-      .catch((error) => expect(error.message).toEqual('Item not found.'));
+  describe('GetInventory', () => {
+    it('should return an inventory of a given id', async () => {
+      prisma.inventory.findUnique = jest
+        .fn()
+        .mockReturnValue({ itemID: '1', itemName: 'item1', quantity: 2 });
+      expect((await spyInventoryService.GetInventory(1)).itemID).toBeDefined();
+      expect(prisma.inventory.findUnique).toHaveBeenCalled();
+    });
+    it('should throw an error item not found with itemID=10000', async () => {
+      prisma.inventory.findUnique = jest.fn().mockReturnValue(null);
+      return await spyInventoryService
+        .GetInventory(10000)
+        .catch((error) => expect(error.message).toEqual('Item not found.'));
+    });
+  });
+  describe('UpdateInventory', () => {
+    it('should return an updatedInventory of a given id and update data', async () => {
+      prisma.inventory.update = jest
+        .fn()
+        .mockReturnValue({ itemID: '12', itemName: 'item1', quantity: 2 });
+      await spyInventoryService.UpdateInventory({
+        itemID: 12,
+        itemName: 'item1',
+        quantity: 2,
+      });
+      expect(prisma.inventory.update).toHaveBeenCalled();
+    });
   });
 });
