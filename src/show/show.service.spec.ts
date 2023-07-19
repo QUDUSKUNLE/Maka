250fb01bbc269af88c7465f448baf70218b7d27a
QUDUSKUNLE <quduskunle@gmail.com>
2023-07-19 14:14:38 +0100

    Abstract mock data

--- src/show/show.service.spec.ts
+++ src/show/show.service.spec.ts
@@ -2,20 +2,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ShowService } from './show.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { InventoryService } from '../inventory/inventory.service';
+import { mockData } from '../mock/mock.data';
 import { NotFoundException } from '@nestjs/common';
 
 describe('ShowService', () => {
-  const createShow = {
-    createShow: [{ showName: 'Show 1' }, { showName: 'Show 2' }],
-  };
-  const soldItemParams = { item_ID: '1', show_ID: '1' };
-  const soldItemShowNotFound = { item_ID: '1', show_ID: '1000000' };
-  const soldItemDto = {
-    quantity: 0,
-  };
-  const soldItemQuantityDto = {
-    quantity: 1,
-  };
   let spyShowService: ShowService;
   let prisma: PrismaService;
   let spyInventoryService: InventoryService;
@@ -35,16 +25,19 @@ describe('ShowService', () => {
   describe('CreateShow', () => {
     it('should create possible number of shows', async () => {
       prisma.show.createMany = jest.fn().mockReturnValue({ count: 2 });
-      const show = await spyShowService.CreateShow(createShow);
-      expect(show.count).toStrictEqual(createShow.createShow.length);
+      const show = await spyShowService.CreateShow(
+        mockData.SHOW_SERVICE.CreateShow.createShow,
+      );
+      expect(show.count).toStrictEqual(
+        mockData.SHOW_SERVICE.CreateShow.createShow.createShow.length,
+      );
     });
   });
   describe('GetShows', () => {
     it('should be able to return some shows', async () => {
-      prisma.show.findMany = jest.fn().mockReturnValue([
-        { showID: '1', showName: 'Show 1' },
-        { showID: '2', showName: 'Show 2' },
-      ]);
+      prisma.show.findMany = jest
+        .fn()
+        .mockReturnValue(mockData.SHOW_SERVICE.GetShows.findMany);
       const show = await spyShowService.GetShows();
       expect(typeof show).toEqual(typeof []);
     });
@@ -53,7 +46,7 @@ describe('ShowService', () => {
     it('should return an show of a given id', async () => {
       prisma.show.findUnique = jest
         .fn()
-        .mockReturnValue({ showID: 1, showName: 'Show 1' });
+        .mockReturnValue(mockData.SHOW_SERVICE.GetShow.findUnique);
       const show = await spyShowService.GetShow(1);
       expect(show.showID).toBeDefined();
       expect(typeof show.showID).toEqual('number');
@@ -62,106 +55,101 @@ describe('ShowService', () => {
       prisma.show.findUnique = jest.fn().mockReturnValue(null);
       return spyShowService
         .GetShow(10000)
-        .catch((error) => expect(error?.message).toEqual('Show not found.'));
+        .catch((error) =>
+          expect(error?.message).toEqual(mockData.SHOW_SERVICE.SHOW_NOT_FOUND),
+        );
     });
   });
   describe('BuyItem', () => {
     it('should return a bought item from a show', async () => {
       spyInventoryService.GetInventory = jest
         .fn()
-        .mockResolvedValue({ itemID: 1, itemName: 'item 1', quantity: 100 });
+        .mockResolvedValue(mockData.SHOW_SERVICE.BuyItem.GetInventory);
       spyShowService.GetShow = jest
         .fn()
-        .mockReturnValue({ showID: 1, showName: 'show 1' });
+        .mockReturnValue(mockData.SHOW_SERVICE.BuyItem.GetShow);
       spyInventoryService.UpdateInventory = jest
         .fn()
-        .mockResolvedValue({ itemID: 1, itemName: 'item 1', quantity: 101 });
+        .mockResolvedValue(mockData.SHOW_SERVICE.BuyItem.UpdateInventory);
       prisma.soldInventories.create = jest.fn().mockResolvedValue({ count: 1 });
-      await spyShowService.BuyItem(soldItemParams, soldItemQuantityDto);
+      await spyShowService.BuyItem(
+        mockData.SHOW_SERVICE.BuyItem.soldItemParams,
+        mockData.SHOW_SERVICE.BuyItem.soldItemQuantityDto,
+      );
       expect(spyInventoryService.GetInventory).toHaveBeenCalled();
       expect(spyShowService.GetShow).toHaveBeenCalled();
       expect(prisma.soldInventories.create).toHaveBeenCalled();
     });
     it('should throw error with quantity=0', async () => {
       return spyShowService
-        .BuyItem(soldItemParams, soldItemDto)
-        .catch((error) => expect(error?.message).toEqual('Zero order made.'));
+        .BuyItem(
+          mockData.SHOW_SERVICE.BuyItem.soldItemParams,
+          mockData.SHOW_SERVICE.BuyItem.soldItemDto,
+        )
+        .catch((error) =>
+          expect(error?.message).toEqual(mockData.SHOW_SERVICE.ZERO_ORDER_MADE),
+        );
     });
     it('should throw error with quantity more than what is in stock', async () => {
       spyInventoryService.GetInventory = jest
         .fn()
-        .mockResolvedValue({ itemID: 1, itemName: 'item 1', quantity: 2 });
+        .mockResolvedValue(mockData.SHOW_SERVICE.BuyItem.GetInventory);
       return spyShowService
-        .BuyItem(soldItemParams, { quantity: 1000 })
-        .catch((error) => expect(error?.message).toEqual('Item out of stock'));
+        .BuyItem(mockData.SHOW_SERVICE.BuyItem.soldItemParams, {
+          quantity: 1000,
+        })
+        .catch((error) =>
+          expect(error?.message).toEqual(
+            mockData.SHOW_SERVICE.ITEM_OUT_OF_STOCK,
+          ),
+        );
     });
     it('should throw error show not found', async () => {
       spyShowService.BuyItem = jest
         .fn()
-        .mockRejectedValue(new NotFoundException('Show not found.'));
+        .mockRejectedValue(
+          new NotFoundException(mockData.SHOW_SERVICE.SHOW_NOT_FOUND),
+        );
       await expect(
-        spyShowService.BuyItem(soldItemShowNotFound, { quantity: 2 }),
-      ).rejects.toThrowError('Show not found.');
+        spyShowService.BuyItem(
+          mockData.SHOW_SERVICE.BuyItem.soldItemShowNotFound,
+          { quantity: 2 },
+        ),
+      ).rejects.toThrowError(mockData.SHOW_SERVICE.SHOW_NOT_FOUND);
     });
   });
   describe('GetSoldItems', () => {
     it('should return object of bought item', async () => {
-      prisma.soldInventories.findMany = jest.fn().mockResolvedValue([
-        {
-          id: 1,
-          inventoryId: 1,
-          showId: 1,
-          quantity: 2,
-          inventory: { itemID: 1, itemName: 'item1', quantity: 2 },
-        },
-        {
-          id: 2,
-          inventoryId: 1,
-          showId: 1,
-          quantity: 3,
-          inventory: { itemID: 1, itemName: 'item1', quantity: 2 },
-        },
-      ]);
+      prisma.soldInventories.findMany = jest
+        .fn()
+        .mockResolvedValue(
+          mockData.SHOW_SERVICE.GetSoldItems.GetSoldItem.findMany,
+        );
       await spyShowService.GetSoldItems({
         show_ID: '1',
         item_ID: '1',
       });
       spyShowService.GetSoldItems = jest
         .fn()
-        .mockReturnValue({ itemID: '1', itemName: 'Show 1', quantity_sold: 3 });
+        .mockReturnValue(
+          mockData.SHOW_SERVICE.GetSoldItems.GetSoldItem.SpyGetSoldItem,
+        );
       expect(prisma.soldInventories.findMany).toHaveBeenCalled();
     });
     it('should return array of bought items', async () => {
-      prisma.soldInventories.findMany = jest.fn().mockResolvedValue([
-        {
-          id: 1,
-          inventoryId: 1,
-          showId: 1,
-          quantity: 2,
-          inventory: { itemID: 1, itemName: 'item1', quantity: 2 },
-        },
-        {
-          id: 2,
-          inventoryId: 2,
-          showId: 1,
-          quantity: 3,
-          inventory: { itemID: 2, itemName: 'item2', quantity: 4 },
-        },
-        {
-          id: 3,
-          inventoryId: 2,
-          showId: 1,
-          quantity: 4,
-          inventory: { itemID: 2, itemName: 'item2', quantity: 5 },
-        },
-      ]);
+      prisma.soldInventories.findMany = jest
+        .fn()
+        .mockResolvedValue(
+          mockData.SHOW_SERVICE.GetSoldItems.GetSoldItems.findMany,
+        );
       await spyShowService.GetSoldItems({
         show_ID: '1',
       });
-      spyShowService.GetSoldItems = jest.fn().mockReturnValue([
-        { item_ID: '1', itemName: 'Show 1', quantity_sold: 3 },
-        { item_ID: '2', itemName: 'Show 2', quantity_sold: 3 },
-      ]);
+      spyShowService.GetSoldItems = jest
+        .fn()
+        .mockReturnValue(
+          mockData.SHOW_SERVICE.GetSoldItems.GetSoldItems.SpyGetSoldItems,
+        );
       expect(prisma.soldInventories.findMany).toHaveBeenCalled();
     });
   });
